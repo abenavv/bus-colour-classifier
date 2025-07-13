f943795c76249803a3b94412ed6534f5f707c058
abenavv <abhinavdec2@gmail.com>
2025-07-13 19:21:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,4 +28,12 @@ if uploaded_file is not None:
     # Preprocess image
     img = image.resize((224, 224))
     img_array = np.array(img) / 255.0  # scale to [0,1]
-    img_array = np.expand_dims(img_ar_
+    img_array = np.expand_dims(img_array, axis=0)  # make it (1, 224, 224, 3)
+
+    # Predict
+    predictions = model.predict(img_array)
+    predicted_class = class_names[np.argmax(predictions)]
+
+    st.write(f"### Predicted Colour: **{predicted_class}**")
+    st.write(f"Prediction Probabilities: {predictions}")
+
